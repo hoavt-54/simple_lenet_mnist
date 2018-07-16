f476ccc7503dd6ef37f04b8933817ee81b88ad42
Hoa Vu <hoavu@gmail.com>
2018-07-16 18:04:21 +0200

    fix some comments

--- mnist_assignment.py
+++ mnist_assignment.py
@@ -4,9 +4,19 @@ import tensorflow.contrib.slim as slim
 from tensorflow.examples.tutorials.mnist import input_data
 
 """
+LetNet-5
+http://yann.lecun.com/exdb/publis/pdf/lecun-01a.pdf
+
 The code in this submission implements a simple convolutional neural network containing 2 conv layers 
 which then followed by a fully connected layer. Each conv layer consists of a conv op and an pooling op.
-I use maxpooling as pooling ops in this assignment. 
+I use maxpooling as pooling ops in this assignment. I also use TF-slim which reduce time of defining weights and biases. 
+
+This implementation differs from the original architecture as well as added some modern findings in deep learning
+1. making use of dropout
+2. Relu instead of sigmoid
+3. training by cross-entropy and AdamOptimizer instead of log-likelihood(eq Mean Square Error) and 
+
+
 """
 
 
@@ -17,18 +27,20 @@ def lenet(X, is_training=True):
     """
     #reshape X as it is flat array in mnist dataset
     X = tf.reshape(X, [-1, 28,28,1])
-    #first conv layer
-    conv1 = slim.conv2d(X, 4, [3, 3], scope='conv1') #shape [None, 28, 28, 16]
-    mp1 = slim.max_pool2d(conv1, [2, 2], scope='pool1') #shape [None, 14, 14, 16]
+    #first conv layer, same padding by default
+    conv1 = slim.conv2d(X, 6, [3, 3], scope='conv1') #shape [None, 28, 28, 6]
+    mp1 = slim.max_pool2d(conv1, [2, 2], scope='pool1') #shape [None, 14, 14, 6]
     #second conv layer
-    conv2 = slim.conv2d(mp1, 6, [3, 3], scope='conv2') #shape [None, 28, 28, 32]
-    mp2 = slim.max_pool2d(conv2, [2, 2], scope='pool2') #shape [None, 7, 7, 32]
+    conv2 = slim.conv2d(mp1, 16, [3, 3], scope='conv2') #shape [None, 28, 28, 16]
+    mp2 = slim.max_pool2d(conv2, [2, 2], scope='pool2') #shape [None, 7, 7, 16]
     #last fully connected layer
     mp2_flatten = tf.contrib.layers.flatten(mp2)
-    fc = slim.fully_connected(mp2_flatten, 120, scope='fc1') #shape[None, 512]
-    drp = slim.dropout(fc, 0.5, is_training=is_training, scope='dropout1')
+    fc1 = slim.fully_connected(mp2_flatten, 120, scope='fc1') #shape[None, 120]
+    drp = slim.dropout(fc1, 0.6, is_training=is_training, scope='dropout1')
+    fc2 = slim.fully_connected(fc1, 84, scope='fc2') #shape[None, 84]
+    drp = slim.dropout(fc2, 0.6, is_training=is_training, scope='dropout2')
     #output layer, no activation fn because there will be a softmax later when calculaing the loss
-    out = slim.fully_connected(drp, 10, activation_fn=None, scope='fc2') #shape[None, 10]
+    out = slim.fully_connected(drp, 10, activation_fn=None, scope='fc3') #shape[None, 10]
     return out 
 
 def train(mnist, lr=0.0001, num_epochs=8000, train_log=True):
@@ -54,7 +66,7 @@ def train(mnist, lr=0.0001, num_epochs=8000, train_log=True):
                 train_accuracy = accuracy.eval(feed_dict={ x: batch[0], y: batch[1], is_training:False}) 
                 print('Training accuracy', train_accuracy)
             train_step.run(feed_dict={x: batch[0], y: batch[1], is_training:True})
-        print('Testing accuracy', accuracy.eval(feed_dict={
+        print(1 - accuracy.eval(feed_dict={
             x: mnist.test.images, y: mnist.test.labels, is_training:False}))
 
 if __name__ == '__main__':
